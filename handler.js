65eb8970f7fbe3a12c176620ace393395f28bac2
Steve Winton <stevewinton@gmail.com>
2018-09-20 17:14:02 -0500

    Following the quickstart guide
    https://serverless.com/framework/docs/providers/aws/guide/quick-start/

---
+++ handler.js
@@ -0,0 +1,14 @@
+
+
+module.exports.hello = async (event, context) => 
+   ({
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  })
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+;
